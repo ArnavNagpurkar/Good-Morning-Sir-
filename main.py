38cebaaaea394829b7a9ac10e561ad33169db945
ArnavNagpurkar <84492896+ArnavNagpurkar@users.noreply.github.com>
2023-03-04 14:07:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# Display morning, afternoon, evening or night
+
+import time
+timestamp = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))
+minute = int(time.strftime('%M'))
+second = int(time.strftime('%S'))
+
+# Display the current time
+print("The time is:", timestamp)
+
+# print good morning/evening/afternoon/night
+if (hour > 4 and hour <= 12):
+    print("Good Morning Sir!")
+elif (hour > 12 and hour < 17):
+    print("Good Afternoon Sir!")
+elif (hour > 17 and hour < 19):
+    print("Good Evening Sir!")
+else:
+    print("Good Night Sir!")
+
+# End...
